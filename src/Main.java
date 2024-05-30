48d5ae101bc3b2ac21a7e07d7040abb580f3a9ac
DJWSN <1151964901@qq.com>
2024-05-30 14:21:52 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("系统启动");
         Scanner in = new Scanner(System.in);
         Library myLittleLibrary = new Library();
         //输入图书馆中书籍的总数
